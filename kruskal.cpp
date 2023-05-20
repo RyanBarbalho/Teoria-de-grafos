cc43f020bba0c6986c4a3a8586e750082936c6ce
Ryan, aka wayan <106884891+RyanBarbalho@users.noreply.github.com>
2023-05-20 16:57:13 -0300

    Update kruskal.cpp
    kruskal puro com entrada pelo terminal funcionando

--- kruskal.cpp
+++ kruskal.cpp
@@ -33,18 +33,33 @@ class grafo{
         }
         void addAresta(int u, int v, int w){
             //vertice u vai para o vertice v com peso w
+            for(const auto& edge: adj[u]){
+                if(edge.first == v){
+                    return;
+                }
+            }
             adj[u].push_back(make_pair(v, w));
         }
 
         //union-find
         int find(int u, vector<int> &pai){
-            if (pai[u] != u){
-                return find(pai[u], pai);
-            }
-            else{
-                return pai[u];
-            }
+        if (pai[u] != u){
+            return find(pai[u], pai);
         }
+        else{
+            return u;
+        }
+        }
+        //possivel conserto de find
+        // int find(int u, vector<int> &pai){
+        //     if (pai[u] != u){
+        //         return find(pai[u], pai);
+        //     }
+        //     else{
+        //         return u;
+        //     }
+        // }
+        
         //union
         void uniao(int u, int v, vector<int> &pai, vector<int> &rank){
             int origem = find(u, pai);
@@ -61,11 +76,6 @@ class grafo{
             }
             
         }
-        void make_set(int x, vector<int> &pai, vector<int> &rank)
-        {
-            pai[x] = x;
-            rank[x] = 0;
-        }
         static bool comparacao(const aresta& a, const aresta& b){
             return a.peso < b.peso;
         }
@@ -77,39 +87,40 @@ class grafo{
             vector<aresta> agm; // agm = T
             //vetor de arestas que serao ordenadas por peso
             vector<aresta> arestas; 
+            //(make_set)
             //vetor que armazena o vertice pai de cada vertice
             vector<int> pai(V);
             //vetor que armazena o rank de cada vertice
             vector<int> rank(V,0);
             
             //criar vetor com todas as arestas e pesos
+            // i representa cada vertice
+            cout<< "arestas: " << endl;
             for(int i = 0; i< V; i++){
                 for(auto it = adj[i].begin(); it != adj[i].end(); it++){
                     int dst = (*it).first;
                     int pso = (*it).second;
-                    //se o vertice destino for maior que o vertice origem
-                    if(i < dst){
-                        aresta a;
-                        a.destino = dst;
-                        a.peso = pso;
-                        a.origem = i+1;
-                        arestas.push_back(a);
-                        cout << arestas[i].origem << " " << arestas[i].destino << " "
-                        << arestas[i].peso << endl;
+                    //evitar que duplique as arestas
+                    if(dst < i){
+                        continue;
                     }
+                    aresta a;
+                    a.origem = i;
+                    a.destino = dst;
+                    a.peso = pso;
+                    arestas.push_back(a);
                 }
             }
             //ordenar as arestas em ordem crescente em relaçao ao peso
             
-            sort(arestas.begin(),arestas.end(), comparacao);
-        
-            //inicializa o vetor pai com -1 em todas as posiçoes
-            //make_set
-            for(int i=1; i<V; i++){
-                make_set(i, pai, rank);
+            //make set pai
+            for (int i=0; i<arestas.size(); i++){
+                pai[i] = i;
+                //rank ja esta definido 0 para todos
             }
-        
-            
+
+            sort(arestas.begin(),arestas.end(), comparacao);
+              
             //arvore geradora minima
             //percorre todas as aresta com o peso sorteado (ordem crescente)
             for (int i = 0; i< arestas.size(); i++){
@@ -120,11 +131,9 @@ class grafo{
                     //adiciona a aresta na arvore
                     //T = T U {(u,v)}
                     agm.push_back(arestas[i]);
-                    uniao(u, v, pai, rank);
-                    
+                    uniao(u, v, pai, rank);   
                 }
             }
-            cout << "cabou kruskall" << endl;
             return agm;
 
         }
@@ -139,7 +148,8 @@ int main(){
     cout << "Digite o numero de vertices e arestas: " << endl;
     cin >> VERTEX >> EDGES;
     cout << "-------------------" << endl;
-    grafo g(VERTEX);
+    //vertex + 1 pq começa por 1, nao 0.
+    grafo g(VERTEX+1);
     
     cout<< "insira os vertice u -> vertice v-> peso w" << endl;
     for(int i=0; i<EDGES; i++){
@@ -147,17 +157,15 @@ int main(){
         cin >> u >> v >> w;
         g.addAresta(u, v, w);
         g.addAresta(v, u, w);
-        cout << "-------------------" << endl;
     
     }
     cout << "-------------------" << endl;
     vector<aresta> agm = g.kruskall();
 
     cout<< "saida kruskall " << endl;
-    for (int i=0; i< VERTEX ; i++){
-        cout << "(" << agm[i].origem << " - " << agm[i].destino << ") ";
+    for (const auto& aresta : agm){
+        cout << "(" << aresta.origem << " - " << aresta.destino << ") ";
     }
     cout << endl;
 }
-
 
